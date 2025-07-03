51168e74a858b2213f83514c9f660ec8fb2b0b81
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-03 18:57:35 -0300

    cree las rutas de inicio, error, admin y login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Menu from "./shared/Menu";
-import { BrowserRouter } from "react-router";
-import Footer from "./shared/Footer";
+import Menu from "./components/shared/Menu";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Footer from "./components/shared/Footer";
+import Administrador from "./components/pages/Administrador";
+import Error404 from "./components/pages/Error404";
+import Inicio from "./components/pages/Inicio";
+import Login from "./components/pages/Login";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Menu></Menu>
-    <main className="container my-3">
-      <h1 className="text-center">Hola Mundo</h1>
-      </main>
-      <Footer></Footer>
+      <BrowserRouter>
+        <Menu></Menu>
+        <main className="container my-3">
+          <Routes>
+            <Route path="/" element={<Inicio></Inicio>}></Route>
+            <Route
+              path="/login"
+              element={<Login></Login>}
+            ></Route>
+            <Route
+              path="/administrador"
+              element={<Administrador></Administrador>}
+            ></Route>
+            <Route path="*" element={<Error404></Error404>}></Route>
+          </Routes>
+        </main>
+        <Footer></Footer>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
