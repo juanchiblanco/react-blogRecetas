7849c005db7735c4af3dd23c51577a77f6314887
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-03 18:29:11 -0300

    agregue maquetado del menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Menu from "./shared/Menu";
+import { BrowserRouter } from "react-router";
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Menu></Menu>
       <h1>Hola Mundo</h1>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/shared/Menu.jsx
@@ -0,0 +1,29 @@
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const Menu = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand  href="/">
+            <div className="d-flex gap-2 mt-2">
+                <p className="lead fs-4">TuReceta</p>
+                <i className="bi bi-fork-knife mt-1"></i>
+            </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink className="nav-link">Inicio</NavLink>
+            <NavLink className="nav-link">Administrador</NavLink>
+            <Button className="nav-link">Logout</Button>
+            <NavLink className="nav-link">Login</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Menu;
