3c811335cca8515633279c4a483979ba49ad94a2
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-07 23:49:42 -0300

    agregue iconos de estrellas al inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -12,7 +12,14 @@ const Inicio = () => {
       </article>
         <Container className="my-5">
         <h1 className="display-4 text-center">Nuestras Recetas, tu tranquilidad</h1>
-        <p className="raleway text-center"><strong>Ya seas principiante o chef</strong>, aquí encontrarás algo para ti.</p>
+        <p className="raleway text-center"><strong>Ya seas principiante o chef</strong>, aquí encontrarás lo que buscas.</p>
+        <div className="d-flex justify-content-center gap-3">
+          <i className="bi bi-star-fill text-warning fs-3"></i>
+          <i className="bi bi-star-fill text-warning fs-3"></i>
+          <i className="bi bi-star-fill text-warning fs-3"></i>
+          <i className="bi bi-star-fill text-warning fs-3"></i>
+          <i className="bi bi-star-fill text-warning fs-3"></i>
+        </div>
         <hr />
         <h4 className="mt-5">Explora una colección de recetas fáciles, sabrosas y para todos los niveles.</h4>
         </Container>
