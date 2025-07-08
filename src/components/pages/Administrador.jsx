523310da30f5e8b8d6948e255032982a6d3de307
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-08 00:42:35 -0300

    maquetado de admin

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import { Button, Table } from "react-bootstrap";
 
 const Administrador = () => {
     return (
+        <section className="container mainSection">
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="display-4 tinos">Mis Recetas</h1>
         <div>
-            
+        <Button className="btn btn-success" >
+          <i className="bi bi-file-earmark-plus"></i>
+        </Button>
+        <Button className="btn btn-info ms-2" >
+          <i className="bi bi-database-fill-add"></i>
+        </Button>
         </div>
+      </div>
+      <hr />
+      <Table responsive striped bordered hover className="raleway">
+        <thead>
+          <tr className="text-center">
+            <th>ID</th>
+            <th>Receta</th>
+            <th>Precio</th>
+            <th>URL de Imagen</th>
+            <th>Categoria</th>
+            <th>Nivel</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </Table>
+    </section>
     );
 };
 
