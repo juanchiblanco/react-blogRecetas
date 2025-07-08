3f5a1fda7e5dc960f8bd9f8388535c26d474c020
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-08 00:13:59 -0300

    maquetado de error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
 const Error404 = () => {
     return (
-        <div>
-            
-        </div>
+
+        <section className="text-center container">
+      <img src='https://cdn-icons-png.flaticon.com/512/755/755014.png' alt="error 404" className='img-fluid'/>
+      <div className='my-4'>
+        <button className="btn btn-warning" >Volver al inicio</button>
+      </div>
+    </section>
     );
 };
 
