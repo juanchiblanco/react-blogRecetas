07408bc36bfd5dd0e407d2bef7ddf6936e6d5f6e
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-16 01:32:40 -0300

    agregue la ruta a incio en la pagina error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,13 +1,22 @@
-const Error404 = () => {
-    return (
+import { useNavigate } from "react-router";
 
-        <section className="text-center container">
-      <img src='https://cdn-icons-png.flaticon.com/512/755/755014.png' alt="error 404" className='img-fluid'/>
-      <div className='my-4'>
-        <button className="btn btn-warning" >Volver al inicio</button>
+const Error404 = () => {
+  const navegacion = useNavigate();
+
+  return (
+    <section className="text-center container">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/755/755014.png"
+        alt="error 404"
+        className="img-fluid"
+      />
+      <div className="my-4">
+        <button className="btn btn-warning" onClick={() => navegacion("/")}>
+          Volver al inicio
+        </button>
       </div>
     </section>
-    );
+  );
 };
 
 export default Error404;
