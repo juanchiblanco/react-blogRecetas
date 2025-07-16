6d891f4af91ae9a6468089a0a8d24a735a3f56d6
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-16 00:50:08 -0300

    agregue mas estilo a las cards, cambie texto del boton y agregue la duracion

--- src/components/pages/producto/CardReceta.jsx
+++ src/components/pages/producto/CardReceta.jsx
@@ -13,15 +13,25 @@ const CardReceta = ({receta}) => {
           />
         </div>
         <Card.Body className="h-100">
-          <Card.Title className="primary-font text-center tinos">{receta.formPlato}</Card.Title>
+          <Card.Title className="text-center tinos fs-4">{receta.formPlato}</Card.Title>
+          <hr />
           <Card.Text className="text-center raleway">
             {receta.formDescripcionBreve} <br className="mb-2" />
           </Card.Text>
-          <Card.Text className="fw-bold text-warning text-center">{receta.formDificultad}</Card.Text>
+          <div className="d-flex justify-content-around bg-dark-subtle p-3">
+            <div>
+              <Card.Text>Duración</Card.Text>
+            <Card.Text className="fw-bold text-warning text-center">{receta.formDuracion}"</Card.Text>
+            </div>
+            <div>
+              <Card.Text>Dificultad</Card.Text>
+            <Card.Text className="fw-bold text-warning text-center">{receta.formDificultad}</Card.Text>
+            </div>
+          </div>
         </Card.Body>
         <Card.Footer className="text-center">
           <Link variant="success" className="w-50 btn btn-success" to={`/detalle.html?id=${receta.id}`}>
-            Ver más
+            Ver receta
           </Link>
         </Card.Footer>
       </Card>
