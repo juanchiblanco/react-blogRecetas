884e27e242580fbcc2aa5dd1aaec56bb8428a847
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-16 01:28:44 -0300

    cree un condicional que uestra la apginacion solo si hay recetas en localstorage

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,7 +3,6 @@ import CardReceta from "./producto/CardReceta";
 import Paginacion from "./Paginacion";
 
 const Inicio = () => {
-
   const recetasLocalStorage = JSON.parse(localStorage.getItem("recetas")) || [];
 
   return (
@@ -15,9 +14,14 @@ const Inicio = () => {
           alt="Plato de comida"
         />
       </article>
-        <Container className="my-5">
-        <h1 className="display-4 text-center">Nuestras Recetas, tu tranquilidad</h1>
-        <p className="raleway text-center"><strong>Ya seas principiante o chef</strong>, aquí encontrarás lo que buscas.</p>
+      <Container className="my-5">
+        <h1 className="display-4 text-center">
+          Nuestras Recetas, tu tranquilidad
+        </h1>
+        <p className="raleway text-center">
+          <strong>Ya seas principiante o chef</strong>, aquí encontrarás lo que
+          buscas.
+        </p>
         <div className="d-flex justify-content-center gap-3">
           <i className="bi bi-star-fill text-warning fs-3"></i>
           <i className="bi bi-star-fill text-warning fs-3"></i>
@@ -26,14 +30,26 @@ const Inicio = () => {
           <i className="bi bi-star-fill text-warning fs-3"></i>
         </div>
         <hr />
-        <h4 className="mt-5" id="explorar">Explora una colección de recetas fáciles, sabrosas y para todos los niveles.</h4>
+        <h4 className="mt-5" id="explorar">
+          Explora una colección de recetas fáciles, sabrosas y para todos los
+          niveles.
+        </h4>
         <Row>
-          {
-            recetasLocalStorage.map((receta)=> <CardReceta receta={receta} key={receta.id}></CardReceta>)
-          }
+          {recetasLocalStorage.map((receta) => (
+            <CardReceta receta={receta} key={receta.id}></CardReceta>
+          ))}
         </Row>
-        <div className="d-flex justify-content-center mt-5"><Paginacion></Paginacion></div>
-        </Container>
+        {recetasLocalStorage.length === 0 ? (
+          <p className="text-center fs-5 mt-4">
+            <i className="bi bi-x display-3"></i>
+            <br></br>No hay recetas agregadas
+          </p>
+        ) : (
+          <div className="d-flex justify-content-center mt-5">
+            <Paginacion></Paginacion>
+          </div>
+        )}
+      </Container>
     </section>
   );
 };
