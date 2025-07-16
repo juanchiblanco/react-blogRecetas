b8aea534af995fad9168994a061ee3bfed5bfebe
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-16 17:59:41 -0300

    agregue estiloa  la lista de pasos

--- src/components/pages/producto/ItemPaso.jsx
+++ src/components/pages/producto/ItemPaso.jsx
@@ -2,10 +2,10 @@ import { Button, ListGroup } from "react-bootstrap";
 
 const ItemPaso = ({ paso, borrarPaso, indice }) => {
   return (
-    <ListGroup.Item className="d-flex my-2">
+    <ListGroup.Item className="d-flex my-2 border-bottom border-light-subtle">
       {indice}. {paso}
       <Button
-      className="ms-auto btn-sm"
+      className="ms-auto btn-sm mb-2"
         type="button"
         variant="danger"
         onClick={() => borrarPaso(paso)}
