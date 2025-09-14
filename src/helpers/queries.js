440157eefa8dac7f1100842d21aab4311d0d67f9
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-09-14 18:35:05 -0300

    agregue funcion de login a las queries

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -70,3 +70,19 @@ export const borrarRecetaID = async (id) => {
     return null;
   }
 };
+
+export const login = async (usuario) => {
+  try {
+    const respuesta = await fetch(urlUsuarios + "/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
